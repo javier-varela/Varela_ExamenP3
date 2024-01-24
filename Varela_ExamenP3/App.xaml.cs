ee2895f18574ab0c23100a34325b6cc3fcb8a319
javier.varela <javier.varela@udla.edu.ec>
2024-01-24 07:09:35 -0500

    Add project files.

---
+++ Varela_ExamenP3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Varela_ExamenP3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Varela_ExamenP3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Varela_ExamenP3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
